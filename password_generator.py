d6b30e29f9ce2e6258335f9323e16f1cab828854
stzekov <stanislav.tzekov@dxc.com>
2024-09-08 23:03:48 +0300

    Refactor password generator file.

--- password_generator.py
+++ password_generator.py
@@ -5,24 +5,20 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 
 caps_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
                 'V', 'W', 'X', 'Y', 'Z']
+
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '_', '@', '~', '^']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 
-def insert_random(number, chars_collection, password_chars):
-    """Inserts random number of chars in password collection."""
-    for i in range(number):
-        password_chars.append(random.choice(chars_collection))
-
-
 def generator():
     """Generates random password by given sequence of chars."""
-    password_chars = []
 
-    insert_random(4, letters, password_chars)
-    insert_random(4, caps_letters, password_chars)
-    insert_random(2, symbols, password_chars)
-    insert_random(2, numbers, password_chars)
+    random_lower_letters = [random.choice(letters) for _ in range(random.randint(3, 5))]
+    random_upper_letters = [random.choice(caps_letters) for _ in range(random.randint(3, 5))]
+    random_symbols = [random.choice(symbols) for _ in range(random.randint(1, 2))]
+    random_numbers = [random.choice(numbers) for _ in range(random.randint(1, 2))]
+
+    password_chars = random_lower_letters + random_upper_letters + random_symbols + random_numbers
 
     randomized_password = password_chars[:]
     random.shuffle(randomized_password)

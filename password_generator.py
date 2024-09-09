4103c81517044bf2a73652535378cf6c7f9bbd0b
stzekov <stanislav.tzekov@dxc.com>
2024-09-09 09:29:01 +0300

    Update password-generator

--- password_generator.py
+++ password_generator.py
@@ -1,4 +1,4 @@
-import random
+from random import choice, randint, shuffle
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
            'w', 'x', 'y','z']
@@ -13,13 +13,13 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 def generator():
     """Generates random password by given sequence of chars."""
 
-    password_lower_letters = [random.choice(letters) for _ in range(random.randint(3, 5))]
-    password_upper_letters = [random.choice(caps_letters) for _ in range(random.randint(3, 5))]
-    password_symbols = [random.choice(symbols) for _ in range(random.randint(1, 2))]
-    password_numbers = [random.choice(numbers) for _ in range(random.randint(1, 2))]
+    password_lower_letters = [choice(letters) for _ in range(randint(3, 5))]
+    password_upper_letters = [choice(caps_letters) for _ in range(randint(3, 5))]
+    password_symbols = [choice(symbols) for _ in range(randint(1, 2))]
+    password_numbers = [choice(numbers) for _ in range(randint(1, 2))]
 
     password_list = password_lower_letters + password_upper_letters + password_symbols + password_numbers
 
-    random.shuffle(password_list)
+    shuffle(password_list)
 
     return "".join(password_list)
